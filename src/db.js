5a408e01efe2317dc644492c9152e40dda948663
tablonimus <lautamartin1995@gmail.com>
2022-10-05 03:15:41 -0300

    paradep

--- src/db.js
+++ src/db.js
@@ -2,13 +2,17 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DATABASE_URL } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/fbiwanted`,
+  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/fbiwanted`,
+  DATABASE_URL,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false },
+    },
   }
 );
 const basename = path.basename(__filename);
@@ -38,8 +42,6 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Criminal } = sequelize.models;
-
-
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
